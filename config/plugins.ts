fc1781cdcfdb697b80de4e524827c5cbae1f4ff5
Dario0318 <drfq180305@gmail.com>
2025-07-03 09:37:08 -0500

    revision de error

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,3 +1,9 @@
+console.log('🌩️ CLOUDINARY CONFIG:', {
+  name: process.env.CLOUDINARY_NAME,
+  key: process.env.CLOUDINARY_KEY,
+  secret: process.env.CLOUDINARY_SECRET ? '[OK]' : '[MISSING]',
+});
+
 export default () => ({
   'users-permissions': {
     enabled: true,
