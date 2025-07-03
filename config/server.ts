4c025946866a5b34ca7d7cc6e38996a601a1bfaf
Dario0318 <drfq180305@gmail.com>
2025-07-03 10:28:11 -0500

    nueva variable agregada

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env("http://localhost:1337","https://backend-tecnogo.onrender.com"),
+  url: env("PUBLIC_URL","https://backend-tecnogo.onrender.com"),
   app: {
     keys: env.array('APP_KEYS'),
   },
